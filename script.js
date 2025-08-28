75c18d02e18e4ad1cba4ef8a1c81ce05dc576452
NurahadSarker <nurahadrobi451@gmail.com>
2025-08-28 10:30:26 +0600

    added some changes

--- script.js
+++ script.js
@@ -28,9 +28,32 @@ document.getElementById("call-btn-1").addEventListener('click', function(){
 
     const historyData = {
         name: "National Emergency Number",
+        number: 999,
         date: new Date().toLocaleTimeString(),
     }
 
     callHistoryData.push(historyData)
     console.log(callHistoryData)
+    const callHistory = document.getElementById('call-history-container')
+    callHistory.innerText = ""
+    for(const data of callHistoryData){
+        if(coinValue > 0){
+            const div = document.createElement("div")
+            div.innerHTML = `
+            <div class="flex items-center justify-between bg-[#FAFAFA] p-[10px] rounded-lg mt-[15px]">
+                <div class="w-[170px]">
+                    <h1 class="text-[16px] font-semibold">${data.name}</h1>
+                    <p class="text-[16px] text-[#5C5C5C]">${data.number}</p>
+                </div>
+                <p>${data.date}</p>
+            </div>        
+        `
+        callHistory.appendChild(div)
+        }
+    }
+})
+
+document.getElementById('clearBtn').addEventListener('click', function(){
+    const callHistory = document.getElementById('call-history-container')
+    callHistory.innerHTML = ""
 })
